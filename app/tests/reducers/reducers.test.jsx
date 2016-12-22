e9af7a5969d11ba0ed9d800edc00408e454988a9
Casey Hutchinson <hutchster@gmail.com>
2016-12-22 09:33:05 -0500

    Add, test showCompleted, searchText reducers

---
+++ app/tests/reducers/reducers.test.jsx
@@ -0,0 +1,28 @@
+var expect = require('expect');
+var df = require('deep-freeze-strict');
+
+var reducers = require('reducers');
+
+describe('Reducers', () => {
+  describe('searchTextReducer', () => {
+    it('should set searchText', () => {
+      var action = {
+        type: 'SET_SEARCH_TEXT',
+        searchText: 'dog'
+      };
+      var res = reducers.searchTextReducer(df(''), df(action));
+      expect(res).toEqual(action.searchText);
+    });     // should set searchText
+  });       // describe searchTextReducer
+
+  describe('showCompletedReducer', () => {
+    it('should update showCompleted', () => {
+      var action = {
+        type: 'TOGGLE_SHOW_COMPLETED'
+      };
+      var res = reducers.showCompletedReducer(df(false), df(action));
+      expect(res).toBe(true);
+    });     // should set searchText
+  });       // describe showCompletedReducer
+
+});         // describe Reducers
