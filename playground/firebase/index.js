6c2806da57aaf35a4275f086a625876308f8d68d
Casey Hutchinson <hutchster@gmail.com>
2016-12-26 02:19:28 -0500

    finishing up Firebase playground

--- playground/firebase/index.js
+++ playground/firebase/index.js
@@ -22,21 +22,83 @@ firebaseRef.set({
     name: 'Casey',
     age: 39
   }
-}).then(() => {
-  console.log('Set worked');
-}, (e) => {
-  console.log('Set failed');
 });
 
-
-// firebaseRef.set({
-//   appName: 'Todo Application'
+// firebaseRef.update({
+//   isRunning: false,
+//   'app/name': 'Todo Applcation'
 // });
 
-firebaseRef.child('user').set({
-  name: 'Mike'
+// firebaseRef.child('app').update({
+//   name: 'Todo Application'
+// }).then(() => {
+//   console.log('Update worked.');
+// }, (e) => {
+//   console.log('Update failed.');
+// });
+
+// firebaseRef.update({
+//   'app/name': 'Todo Application',
+//   'user/name': 'Gandalf'
+// });
+
+// firebaseRef.child('app').update({name: 'Todone Application'});
+// firebaseRef.child('user').update({name: 'Radagast'});
+
+// firebaseRef.remove();
+// firebaseRef.child('app/name').remove();
+
+// firebaseRef.child('app').update({
+//   version: '2.0',
+//   name: null
+// });
+
+// firebaseRef.child('app').once('value').then((snapshot) => {
+//   console.log('Got entire db', snapshot.key, snapshot.val());
+// }, (e) => {
+//   console.log('unable to fetch value');
+// });
+
+// firebaseRef.on('value', (snapshot) => {
+//   console.log('Got value', snapshot.val());
+// });
+//
+// firebaseRef.update({
+//   isRunning: false
+// });
+//
+// firebaseRef.off();
+// // NB: off argument turns off just a particular listener, no arg -> all off
+//
+// firebaseRef.update({
+//   isRunning: true
+// });
+
+var notesRef = firebaseRef.child('notes');
+
+////// basic push:
+// var newNotesRef = notesRef.push();
+// newNotesRef.set({
+//   text: 'Walk the dog.'
+// });
+
+////// chained:
+// var newNotesRef = notesRef.push().set({
+//   text: 'Walk the dog.'
+// });
+
+notesRef.on('child_added', (snapshot) => {
+  console.log('child_added', snapshot.key, snapshot.val());
+});
+notesRef.on('child_changed', (snapshot) => {
+  console.log('child_changed', snapshot.key, snapshot.val());
+});
+notesRef.on('child_removed', (snapshot) => {
+  console.log('child_removed', snapshot.key, snapshot.val());
 });
 
-firebaseRef.child('app').set({
-  name: 'Adobe Photoshop'
+////// push shortcut:
+var newNoteRef = notesRef.push({
+  text: 'Walk the dog.'
 });
+console.log('Todo id', newNoteRef.key);
